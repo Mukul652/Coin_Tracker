d59a102d3b05f2203d066982450999758a7c1943
Mukul652 <82603049+Mukul652@users.noreply.github.com>
2022-08-25 18:48:38 +0530

    uploaded

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey : "AIzaSyAC0GPvGnICOuTewqGWti2fhiXagKiyZD4",
+    authDomain: "coin-tracker-c6295.firebaseapp.com",
+    projectId: "coin-tracker-c6295",
+    storageBucket: "coin-tracker-c6295.appspot.com",
+    messagingSenderId: "934986139380",
+    appId: "1:934986139380:web:758900f9957d33fd1000a1"
+  } ;
+
+  // Initialize Firebase
+  
+  export default firebaseConfig;
